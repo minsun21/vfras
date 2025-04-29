f61fcf3e8d26b5ad2a4d9ccd3475f43b4995374b
minsun21 <mspark0429@daum.net>
2025-04-29 20:10:39 +0900

    modal button 변경

--- src/components/modals/Alert.jsx
+++ src/components/modals/Alert.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
+import Button from "../Button";
 
 Modal.setAppElement("#root");
 
@@ -13,24 +14,23 @@ const Dialog = ({ isOpen, onConfirm, onCancel, message }) => {
       contentLabel="확인 모달"
       style={{
         overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         },
         content: {
-          position: 'relative',
-          inset: 'unset',
-          width: '400px',
-          padding: '20px',
-          borderRadius: '10px',
-          background: 'white',
-          boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
-          textAlign: 'center',
-        }
+          position: "relative",
+          inset: "unset",
+          width: "400px",
+          padding: "20px",
+          borderRadius: "10px",
+          background: "white",
+          boxShadow: "0 2px 10px rgba(0,0,0,0.2)",
+          textAlign: "center",
+        },
       }}
     >
-      <h2>확인하시겠습니까?</h2>
       {message}
       <div
         style={{
@@ -40,19 +40,7 @@ const Dialog = ({ isOpen, onConfirm, onCancel, message }) => {
           gap: "10px",
         }}
       >
-        <button
-          onClick={onConfirm}
-          style={{
-            padding: "10px 10px",
-            backgroundColor: "#4CAF50",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          확인
-        </button>
+        <Button type="confirm" onClick={onCancel} />
       </div>
     </Modal>
   );
--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
+import Button from "../Button";
 
 Modal.setAppElement("#root");
 
@@ -13,24 +14,23 @@ const Dialog = ({ isOpen, onConfirm, onCancel, message }) => {
       contentLabel="확인 모달"
       style={{
         overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         },
         content: {
-          position: 'relative',
-          inset: 'unset',
-          width: '400px',
-          padding: '20px',
-          borderRadius: '10px',
-          background: 'white',
-          boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
-          textAlign: 'center',
-        }
+          position: "relative",
+          inset: "unset",
+          width: "400px",
+          padding: "20px",
+          borderRadius: "10px",
+          background: "white",
+          boxShadow: "0 2px 10px rgba(0,0,0,0.2)",
+          textAlign: "center",
+        },
       }}
     >
-      <h2>확인하시겠습니까?</h2>
       {message}
       <div
         style={{
@@ -40,32 +40,8 @@ const Dialog = ({ isOpen, onConfirm, onCancel, message }) => {
           gap: "10px",
         }}
       >
-        <button
-          onClick={onConfirm}
-          style={{
-            padding: "10px 10px",
-            backgroundColor: "#4CAF50",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          확인
-        </button>
-        <button
-          onClick={onCancel}
-          style={{
-            padding: "10px 10px",
-            backgroundColor: "#f44336",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          취소
-        </button>
+        <Button type="cancel" onClick={onCancel} />
+        <Button type="confirm" onClick={onCancel} />
       </div>
     </Modal>
   );
