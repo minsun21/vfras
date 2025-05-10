a37e1708f54b7547b834000c78eba8a108df7daf
minsun21 <mspark0429@daum.net>
2025-05-10 14:29:11 +0900

    icon 추가

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -19,8 +19,11 @@ const SideBar = () => {
       <nav>
         {MenusConfig.map((group, index) => (
           <div key={`${group.classification}${index}`}>
-            <ul className="menu-group" key={group.classification} >
-              <div className="menu-title" key={group.title}>{group.title}</div>
+            <ul className="menu-group" key={group.classification}>
+              <div className="menu-title" key={group.title}>
+                {group.icon}
+                {group.title}
+              </div>
               {group.items.map((item) => (
                 <li
                   key={item.path}
--- src/config/MenusConfig.js
+++ src/config/MenusConfig.js
@@ -1,10 +1,14 @@
 import { lazy } from "react";
 import { ROUTES } from "../constants/routes";
 import { LABELS } from "../constants/Labels";
+import { FaRegUser, FaRegListAlt } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+import { MdContentCopy } from "react-icons/md";
 
 export const MenusConfig = [
   {
     title: LABELS.SUBSCRIBER,
+    icon: <FaRegUser />,
     items: [
       {
         title: LABELS.SUBSCRIBER_VIEW,
@@ -25,6 +29,7 @@ export const MenusConfig = [
   },
   {
     title: LABELS.COUNTRY_NUMBER,
+    icon: <FaRegListAlt />,
     items: [
       {
         title: LABELS.COUNTRY_NUMBER_MANAGE,
@@ -35,6 +40,7 @@ export const MenusConfig = [
   },
   {
     title: LABELS.CONTENTS,
+    icon: <MdContentCopy />,
     items: [
       {
         title: LABELS.SOUNDS_VIEW,
@@ -45,6 +51,7 @@ export const MenusConfig = [
   },
   {
     title: LABELS.ACCOUNT,
+    icon: <CiSettings />,
     items: [
       {
         title: LABELS.ACCOUNT_MANAGE,
