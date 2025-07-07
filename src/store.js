796e3084262de98c6615db61b41ddaab37a59e47
minsun21 <mspark0429@daum.net>
2025-07-07 20:58:26 +0900

    session storage로 변경

--- src/store.js
+++ src/store.js
@@ -1,37 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session"; // ✅ sessionStorage 사용
+
 import authReducer from "./features/authSlice";
 import breadcrumbReducer from "./features/breadcrumbSlice";
 import passwordReducer from "./features/passwordSlice";
 import didPersonalReducer from "./features/didConfigSlice";
 import didAddReducer from "./features/didAddSlice";
 import loadingReducer from "./features/loadingSlice";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // 기본: localStorage
 
-import { combineReducers } from "redux";
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["auth"], // ✅ 유지할 슬라이스만 설정
-};
-
+// auth만 persist
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: persistReducer(
+    {
+      key: "auth",
+      storage: storageSession,
+    },
+    authReducer
+  ),
   breadcrumb: breadcrumbReducer,
   changePassword: passwordReducer,
   didConfig: didPersonalReducer,
   didAdd: didAddReducer,
-  loading : loadingReducer
+  loading: loadingReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // redux-persist 때문에 필요
+      serializableCheck: false,
     }),
 });
 
