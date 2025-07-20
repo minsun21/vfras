ddf3b51e813027135cbf6140dd3bf891de2f7446
minsun21 <mspark0429@daum.net>
2025-07-20 13:52:53 +0900

    접속 이력

--- src/components/modals/AccountDetailLog.jsx
+++ src/components/modals/AccountDetailLog.jsx
@@ -7,7 +7,7 @@ import { ROUTES } from "../../constants/routes";
 import { KEYS } from "../../constants/Keys";
 
 const AccountDetailLog = ({ selectRow }) => {
-  
+
   const [data, setData] = useState([]);
   const [lastAccessTime, setLastAccessTime] = useState("");
 
@@ -15,6 +15,7 @@ const AccountDetailLog = ({ selectRow }) => {
     axios.get(ROUTES.HISTORY_DETAIL(selectRow[KEYS.ADMIN_ID])).then((res) => {
       const result = res.data.resultData;
       setData(result);
+      console.log('res', res)
       setLastAccessTime(result[0][KEYS.LOG_TIME]);
     });
   }, []);
@@ -29,6 +30,8 @@ const AccountDetailLog = ({ selectRow }) => {
           rowSelectionEnabled={false}
           resultLabel={false}
           pageSelect={false}
+          paginationEnabled={false}
+          maxHeight={450}
         />
       </div>
     </div>
