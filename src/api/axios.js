d927a410f9fb19dcb90cb7aca8cf1138167946fe
minsun21 <mspark0429@daum.net>
2025-07-31 19:13:53 +0900

    0731 배포

--- src/api/axios.js
+++ src/api/axios.js
@@ -20,8 +20,8 @@ export const setLoadingHandler = (handler) => {
 };
 
 const instance = axios.create({
-  baseURL: "/web",
-  // baseURL: window.__ENV__?.API_BASE_URL,
+  // baseURL: "/web",
+  baseURL: window.__ENV__?.API_BASE_URL,
   timeout: 10000,
   withCredentials: true,
 });
